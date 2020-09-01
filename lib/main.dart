598a1816b163d72ff47d423d76089138b7b5fc7e
Wilkenson Francois <wilkensonfrancois@gmail.com>
2020-09-01 03:15:17 -0400

    All functional

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,79 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 55.0,
+                backgroundColor: Color(0xffFDCF09),
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/ken.png'),
+                ),
+              ),
+              Text(
+                'Ken Francois',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 35.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+1 215 284 9378',
+                      style: TextStyle(
+                        color: Colors.teal.shade100,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 18.0,
+                      ),
+                    )),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'wilkensonfrancois@gmail.com',
+                    style: TextStyle(
+                        fontSize: 18.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
